0db5898a2d8d7be872d669786aa2adc5dfc1bd1f
Teodora Garasanin <teodora.garashanin@gmail.com>
2020-06-01 16:40:30 +0200

    feat: add basic login ui

---
+++ TvShowsApp/TvShowsApp/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  TvShowsApp
+//
+//  Created by MacBook on 01/06/2020.
+//  Copyright © 2020 Teodora Garasanin. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var paswordTextField: UITextField!
+    
+    // MARK: - View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func loginAction(_ sender: UIButton) {
+        // TODO: Login request call
+    }
+
+}
+
