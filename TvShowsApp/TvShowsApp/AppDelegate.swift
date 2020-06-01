abe6e0d945f7c2cfa1aacb0f3782ea73948a74fb
Teodora Garasanin <teodora.garashanin@gmail.com>
2020-06-01 16:09:18 +0200

    feat: initial project setup

---
+++ TvShowsApp/TvShowsApp/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  TvShowsApp
+//
+//  Created by MacBook on 01/06/2020.
+//  Copyright © 2020 Teodora Garasanin. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+    
+}
+
